791cb4e2b4d735c5f0e5a58cca34ff3a54a700ed
KGB4000 <kester4000@gmail.com>
2013-10-10 16:51:27 -0400

    Generated Pages controller, added two pages, added html

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Omrails::Application.routes.draw do
+
+  get 'about' => 'pages#about'
+
+
+  root :to => "pages#home"
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  Omrails::Application.routes.draw do
-    root 'application#index'
-  end
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
