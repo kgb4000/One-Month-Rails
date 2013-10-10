ec726ad7ef076bf2fe25dffb77c734d64297a6f1
KGB4000 <kester4000@gmail.com>
2013-10-10 15:24:57 -0400

    Def index in app/controllers/application_controller.rb and had rails route to application#index in the config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Omrails::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  omrails::Application.routes.draw do
+    root 'application#index'
+  end
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
